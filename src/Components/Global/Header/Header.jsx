a4f68b7aebe160bf57600c008ee7bdf2a60c3b39
AryanshMahato <aryanshmahato@gmail.com>
2019-12-30 21:11:44 +0530

    Mobile View of Header Completed

--- src/Components/Global/Header/Header.jsx
+++ src/Components/Global/Header/Header.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
+import Logo from '../../../Assets/Logo.svg';
+import TimeRemainingLogo from '../../../Assets/Time-Remaining-Logo.svg';
+
 export default class Header extends Component {
 	render() {
 		return (
-			<div className='main-Header'>
-				<div className='Logo'>
-					<img src='' alt='' />
+			<div className='main-header'>
+				<div className='logo'>
+					<img src={Logo} alt='Logo' />
+				</div>
+				<hr />
+				<div className='time-remaining-for-sale'>
+					<img src={TimeRemainingLogo} alt='Time Remaining Logo' />
+					<div className='time-remaining__annotation'>
+						<span>Hurry Up Sale is going on</span>
+						<span className='time-remaining'>20:15:32</span>
+					</div>
 				</div>
 			</div>
 		);
