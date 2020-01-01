d64dd9b27252c00df52851c1b7a2b42d0ca1161e
AryanshMahato <aryanshmahato@gmail.com>
2020-01-01 20:27:04 +0530

    Mobile View Footer Added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Header from './Components/Global/Header/Header';
 import HeroBox from './Components/HeroBox/HeroBox';
+import FooterMobile from './Components/Global/Footer-Mobile/FooterMobile';
 
 import './CSS/Main.css';
 
@@ -10,6 +11,7 @@ function App() {
 		<div className='App'>
 			<Header />
 			<HeroBox />
+			<FooterMobile />
 		</div>
 	);
 }
---
+++ src/Components/Global/Footer-Mobile/FooterMobile.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function FooterMobile() {
+	return (
+		<div className='locate-us__mobile'>
+			<a href='#'>Locate Us</a>
+		</div>
+	);
+}
