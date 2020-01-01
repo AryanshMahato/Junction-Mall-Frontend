3048aeb2dd806c7c5289e41ba92e728764cbbd80
AryanshMahato <aryanshmahato@gmail.com>
2020-01-01 20:21:10 +0530

    Hero-Box Added

--- src/Components/HeroBox/HeroBox.jsx
+++ src/Components/HeroBox/HeroBox.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
-// import { FaAngleLeft } from 'react-icons/fa';
+import Image1 from '../../Images/1.jpg';
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 
 export default function HeroBox() {
 	return (
 		<div className='hero-box' id='hero-box'>
-			<h1>Hello World</h1>
+			<div id='hero-box__left-arrow'>
+				<IoIosArrowBack />
+			</div>
+			<div id='hero-box__right-arrow'>
+				<IoIosArrowForward />
+			</div>
+			<img src={Image1} alt='Hero-Image' />
 		</div>
 	);
 }
