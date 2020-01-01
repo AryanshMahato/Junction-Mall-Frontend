b1c37bb821383ccfe6bedb870285276dc774200d
AryanshMahato <aryanshmahato@gmail.com>
2020-01-01 19:07:31 +0530

    Fixed: Desktop View

---
+++ src/Components/HeroBox/HeroBox.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { FaAngleLeft } from 'react-icons/fa';
+
+export default function HeroBox() {
+	return <div className='hero-box' id='hero-box'></div>;
+}
