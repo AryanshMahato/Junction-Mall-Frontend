a8b4c0466c291cd1f7aff2eea852f81cedc703e5
AryanshMahato <aryanshmahato@gmail.com>
2020-01-03 12:31:15 +0530

    Menu List Completed

--- src/Components/MenuList/MenuList.jsx
+++ src/Components/MenuList/MenuList.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React from 'react';
 
 export default function MenuList() {
 	return (
-		<div className="menu-lists">
-			<div className="menu-list-1">
-				<div className="food active">Food</div>
-				<div className="shopping">Shopping</div>
+		<div className='menu-lists'>
+			<div className='menu-list-1'>
+				<div className='food active'>Food</div>
+				<div className='shopping'>Shopping</div>
 			</div>
-			<div className="menu-list-2">
-				<div className="events">Events</div>
-				<div className="entertainment">Entertainment</div>
+			<div className='menu-list-2'>
+				<div className='events'>Events</div>
+				<div className='entertainment'>Entertainment</div>
 			</div>
 		</div>
 	);
