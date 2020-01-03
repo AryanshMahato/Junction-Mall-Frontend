6fbf97412d739c52c7df50076d768718fff28547
AryanshMahato <aryanshmahato@gmail.com>
2020-01-03 12:27:20 +0530

    Menu List Partially Completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import Header from './Components/Global/Header/Header';
-import HeroBox from './Components/HeroBox/HeroBox';
-import FooterMobile from './Components/Global/Footer-Mobile/FooterMobile';
+import Header from "./Components/Global/Header/Header";
+import HeroBox from "./Components/HeroBox/HeroBox";
+import FooterMobile from "./Components/Global/Footer-Mobile/FooterMobile";
+import MenuList from "./Components/MenuList/MenuList";
 
-import './CSS/Main.css';
+import "./CSS/Main.css";
 
 function App() {
 	return (
-		<div className='App'>
+		<div className="App">
 			<Header />
 			<HeroBox />
 			<FooterMobile />
+			<MenuList />
+			{/* For Development Only */}
+			<div className="development"></div>
 		</div>
 	);
 }
---
+++ src/Components/MenuList/MenuList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function MenuList() {
+	return (
+		<div className="menu-lists">
+			<div className="menu-list-1">
+				<div className="food active">Food</div>
+				<div className="shopping">Shopping</div>
+			</div>
+			<div className="menu-list-2">
+				<div className="events">Events</div>
+				<div className="entertainment">Entertainment</div>
+			</div>
+		</div>
+	);
+}
